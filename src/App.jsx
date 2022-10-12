468304946317b9d9d72342491ab9418f450bb5d1
Kishan Mandora <kishanmandora7@gmail.com>
2022-10-12 16:39:40 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline">
+      <h1>Hello Earth!</h1>
+    </div>
+  );
+}
+
+export default App;
